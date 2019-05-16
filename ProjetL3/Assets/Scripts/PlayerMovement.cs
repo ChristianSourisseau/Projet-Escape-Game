030808208d3ffd8243378396bd7e0b2b368e0800
Sourisseau Christian <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-05-16 16:29:12 +0200

    Added basic PlayerMovement Script, to be modified

---
+++ ProjetL3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //------------------MOVEMENT USING QWERTY KEYBOARD--------------------
+    
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+
+    // Start is called before the first frame update
+    void Start() => myRigidBody = GetComponent<Rigidbody2D>();
+
+    // Update is called once per frame
+    private void Update()
+    {
+        change = Vector3.zero;
+        //GetAxisRaw for constant speed in stead of startup. (GetAxis)
+         change.x = Input.GetAxisRaw("Horizontal");
+         change.y = Input.GetAxisRaw("Vertical");
+         if(change != Vector3.zero) {
+            MoveCharacter();
+         }
+
+    }
+
+
+ //moving a small amount each frame
+ void MoveCharacter()
+ {
+     myRigidBody.MovePosition(
+         transform.position + change * speed * Time.deltaTime);
+ }
+    //-----------------------------------------------------------------------
+
+
+}
