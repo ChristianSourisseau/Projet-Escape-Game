6b93e95d1adffd40ef7bdb2bd6ee7234ba16c739
Sourisseau Christian <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-05-18 14:16:55 +0200

    Added Inventory Functionnalities, Includes UI (cannot remove item yet)

---
+++ ProjetL3/Assets/Scripts/Inventory Scripts/Inventory.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    
+    #region Singleton
+    public static Inventory instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one Instance of Inventory found");
+            return;
+        }
+
+        instance = this;
+    }
+    #endregion
+
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+
+
+    public List<Item> items = new List<Item>();
+    public int space = 7;
+
+    public bool Add(Item item)
+    {
+        if (items.Count >= space)
+        {
+            Debug.Log("Not enough room");
+            return false;
+        }
+        items.Add(item);
+        if (onItemChangedCallback != null)
+            onItemChangedCallback.Invoke();
+        return true;
+    }
+    public void remove(Item item)
+    {
+        //TODO
+        //if(onItemChangedCallback != null)
+        //onItemChangedCallback.Invoke();
+    }
+
+}
+
+    
