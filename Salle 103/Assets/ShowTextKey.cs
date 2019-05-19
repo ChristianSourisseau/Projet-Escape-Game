e8ee68ef357ae74955c88b1a8712469959fa4a62
Derieux Jean <jean.derieux@etudiant.univ-rennes1.fr>
2019-05-20 00:50:40 +0200

    the player can now take the key, with the key the text's chest changes

---
+++ Salle 103/Assets/ShowTextKey.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowTextKey : MonoBehaviour
+{
+    public GameObject TextKey;
+
+    void Start()
+    {
+        TextKey.SetActive(false);
+    }
+
+	
+	void OnTriggerEnter2D (Collider2D player)
+	{
+		if (player.gameObject.tag == "Player" )
+		{
+			TextKey.SetActive(true);
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D player)
+	{
+		if (player.gameObject.tag == "Player")
+		{
+			TextKey.SetActive(false);
+		}
+	}
+	
+}
---
+++ Salle 103/Assets/Takekey.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Takekey : MonoBehaviour
+{
+    public GameObject key;
+	private bool present;
+	public bool goldkey;
+	
+    void Start()
+    {
+        key.SetActive(true);
+		present = false;
+		goldkey = false;
+    }
+	
+	
+	void OnTriggerEnter2D (Collider2D player)
+	{
+        if (player.gameObject.tag == "Player") 
+        {
+		present = true;
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D player)
+	{
+        if (player.gameObject.tag == "Player") 
+        {
+		present = false;			
+		}
+	}
+	
+	void Update()
+	{
+	
+		if (Input.GetKeyDown(KeyCode.E) && present)
+		{
+		key.SetActive(false);
+		goldkey = true;
+		}
+	}
+	
+	
+}
