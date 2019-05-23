e2b0460b4e7cc961fa1ae184cdcd0f1e80647c23
Sourisseau Christian <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-05-23 23:19:16 +0200

    changed PlayerInteraction Script to work without using Tags. Simplified code overall

--- Escape_Game/Assets/Scripts/Player Scripts/PlayerInteract.cs
+++ Escape_Game/Assets/Scripts/Player Scripts/PlayerInteract.cs
@@ -14,33 +14,25 @@ public class PlayerInteract : MonoBehaviour {
             //DOES SOMETHING WITH OBJECT
             //FOR NOW JUST PICK UP
                 if (curIntObjScript != null)
-                {
-                Inventory.instance.Add(curIntObjScript.item);
-                curIntObj.SendMessage("Pickup"); 
-                }
+                    curIntObjScript.Pickup(); 
         }
     }
     //If collision with Interactable object sets current Interactable Object 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Interactable"))
-        {
             Debug.Log(collision.name);
             curIntObj = collision.gameObject;
             curIntObjScript = curIntObj.GetComponent<Interactable>();
-        }
     }
+
 
     //exiting collision zone sets current Interractable Object to null
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Interactable"))
-        {  
             if (collision.gameObject == curIntObj)
             {
                 curIntObj = null;
                 curIntObjScript = null;
             }
-        }
     }
 }
