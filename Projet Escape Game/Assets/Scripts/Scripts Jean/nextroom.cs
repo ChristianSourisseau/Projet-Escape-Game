8ed3221485af539cef63f0c12d6926682bc7cf0b
Derieux Jean <jean.derieux@etudiant.univ-rennes1.fr>
2019-05-23 04:44:31 +0200

    added new assets and some rooms

---
+++ Projet Escape Game/Assets/Scripts/Scripts Jean/nextroom.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextroom : MonoBehaviour
+{
+	public GameObject Text;
+
+	private bool present;
+	
+    void Start()
+    {
+        Text.SetActive(false);
+		present = false;
+    }
+	
+	
+	void OnTriggerEnter2D (Collider2D player)
+	{
+        if (player.gameObject.tag == "Player") 
+        {
+		present = true;
+		Text.SetActive(true);
+		}
+	}
+	
+	void OnTriggerExit2D (Collider2D player)
+	{
+        if (player.gameObject.tag == "Player") 
+        {
+		present = false;
+        Text.SetActive(false);
+		
+		}
+	}
+	
+	void Update()
+	{
+	
+		if (Input.GetKeyDown(KeyCode.E) && present)
+		{
+			SceneManager.LoadScene(1);
+
+		}
+	}
+	
+	
+}
