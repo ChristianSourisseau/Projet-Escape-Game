ab35985b6c0d016a65049b7dd9d5e803adc71af3
Chevalot_Maxime <gurdilmaxime@gmail.com>
2019-05-24 19:17:00 +0200

    added scene menu 4 buttons only Start run

---
+++ Escape_Game/Assets/Scripts/Menu Scripts/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Menu : MonoBehaviour
+{
+    public void LOAD_GAME()
+    {
+        SceneManager.LoadScene("room001");
+    }
+
+    public void QUIT_GAME()
+    {
+        Application.Quit();
+    }
+
+    public void LOAD_CREDIT()
+    {
+       // SceneManager.LoadScene("salle103"); remplacer salle103 par la scene des credits
+    }
+
+    public void LOAD_OPTION()
+    {
+        // SceneManager.LoadScene("salle103");  remplacer salle103 par la scene des options
+    }
+}
