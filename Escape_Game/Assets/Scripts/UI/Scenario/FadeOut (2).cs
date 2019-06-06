75f32fc1afc701a9c5eb33dded08edc9869e8c4b
Chevalot_Maxime <gurdilmaxime@gmail.com>
2019-06-06 15:04:47 +0200

    added Menu Pause

---
+++ Escape_Game/Assets/Scripts/UI/Scenario/FadeOut (2).cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class OpenPrison : MonoBehaviour
+{
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+        StartCoroutine("FadeIn");
+    }
+
+    IEnumerable FadeIn()
+    {
+        for (float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+   
+}
---
+++ Escape_Game/Assets/Scripts/UI/Scenario/fadeout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public Animator animator;
+
+    /* Start is called before the first frame update
+    public StartFadeOut()
+    {
+        animator.SetTrigger("FadeOut");
+     
+    }
+    */
+}
