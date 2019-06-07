00703f68b1ed6251cbc7c51616e0251f79cf1711
Christian Sourisseau <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-06-07 23:06:23 +0200

    Cleaned the project of unused data. NOTE : problem with Resources folder and the .txt files in build

---
+++ Escape_Game/Assets/Scripts/Objects/Scriptable_Objects/Objet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Objet : ScriptableObject
+{
+    //Each object has a name and a description
+    //this information is stored in a txt file, unique as it's stored with it's instance ID.
+    
+    public TextAsset createText()
+    {
+        int id = this.GetInstanceID();
+        TextAsset txt = new TextAsset();
+        string path = "/Resources/" + id + ".txt";
+
+        //writing file
+        File.WriteAllText(Application.dataPath + path, this.name + '\n' + this.description);
+
+        //getting TextAsset
+        txt = Resources.Load(id.ToString()) as TextAsset;
+        Debug.Assert(txt != null, "creation of txt file for"+this.name+" failed");
+        return txt;
+    }
+
+    public string description = "an object";
+    new public string name = "Object"; //overriding the old definition for name
+    public Sprite icon = null;
+}
