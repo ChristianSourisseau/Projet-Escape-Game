09bb728468c0d10ae1801a8219a5db69ea9645b1
Christian Sourisseau <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-06-23 11:18:20 +0200

    adding missing files

---
+++ Escape_Game/Assets/Scripts/UI/ToolTip.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ToolTip : MonoBehaviour
+{
+    #region Singleton
+    public static ToolTip instance;
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one Instance of Tooltip found");
+            return;
+        }
+        instance = this;
+        gameObject.SetActive(false);
+    }
+    #endregion
+    public void ShowTooltip(Vector3 position, IDescribable description)
+    {
+        gameObject.SetActive(true);
+        gameObject.transform.position = position;
+        gameObject.GetComponentInChildren<Text>().text = description.GetDescription();
+    }
+
+    public void HideTooltip()
+    {
+        gameObject.SetActive(false);
+    }
+}
