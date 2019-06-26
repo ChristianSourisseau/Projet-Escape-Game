e83f8321b1252cbde4653f67d68188f7d577c9d7
Sourisseau Christian <christian.sourisseau@etudiant.univ-rennes1.fr>
2019-06-26 20:29:49 +0200

    Room number on UI unloads when moving to a scene other than 'Salles'

--- Escape_Game/Assets/Scripts/DontDestroyOnLoad.cs
+++ Escape_Game/Assets/Scripts/DontDestroyOnLoad.cs
@@ -1,11 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
+    public static DontDestroyOnLoad instance;
+    private Scene scene;
+
+
     private void Awake()
     {
-        DontDestroyOnLoad(transform.gameObject);
+        //Verifies that there is only one instance of this script.
+        if (instance != null)
+        {
+            GameObject.Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(transform.gameObject);
+        }
+    }
+
+
+    private void Start()
+    {
+        instance = this;
+        //Destroys gameObject when the Scene Menu is active
+    }
+
+    
+
+    private void Update()
+    {
+        scene = SceneManager.GetActiveScene();
+
+        //Verifies that this script is called in the right context
+        if (!CorrectScene(scene))
+        {
+            Debug.Log("good boy");
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    private bool CorrectScene(Scene scene)
+    {
+        return (scene.name == "Salles");
     }
 }
