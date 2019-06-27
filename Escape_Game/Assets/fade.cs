d8069ecd8224f701804008e2fa3fa4428e3ee721
Chevalot_Maxime <gurdilmaxime@gmail.com>
2019-06-27 15:43:49 +0200

    fade out and update settings

---
+++ Escape_Game/Assets/fade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fade : MonoBehaviour
+{
+    public GameObject img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img.SetActive(true);
+    }
+
+
+    public void end()
+    {
+        img.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
